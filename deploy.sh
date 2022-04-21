7a00827b1382c03279c557d1cfa52250b92deabc
Josua Koenig <josua.koenig@herzotec.de>
2022-04-21 15:26:15 +0200

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+BaseDir="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+run() {
+	printHeader
+	DeployChoice
+
+	printFooterExit
+}
+
+DeployChoice () {
+	echo ""
+	echo "Start Deploy? (y/n)"
+	echo -n "  "
+	read UserChoice
+
+	if [ "$UserChoice" = "n" ] || [ "$UserChoice" = "e" ]; then
+		printFooterExit
+		return
+	fi
+  echo ""
+  
+	if [ "$UserChoice" = "y" ]; then
+	  echoWarning " Deploy START"
+    echo ""
+
+	  DeployLive
+	fi
+}
+
+DeployLive () {
+	echo "Version?"
+	echo -n "  "
+	read Version
+
+	echo ""
+	echoCountdown 5
+
+    docker buildx build --platform linux/amd64,linux/arm64 --push -t gandolf90/schoell:$UserChoice .
+}
+
+printHeader () {
+	clear
+	echo ""
+	echoWarning "----------------------------------------------------------------"
+	echoSuccess "---                      Deploy LIVE                         ---"
+	echoWarning "----------------------------------------------------------------"
+	echo ""
+}
+
+printFooterExit() {
+	echo ""
+	echoWarning "----------------------------------------------------------------"
+	echoSuccess "---                E N D   O F   L I N E                     ---"
+	echoWarning "----------------------------------------------------------------"
+	echo ""
+}
+
+echoCountdown() {
+  echoHighlight "You can now cancel within $1 seconds using CTRL-C"
+		echo -n "  "
+		for (( a=$1; a>0; a--)); do
+			echo -n "$a..."
+			sleep 1;
+		done
+	echoSuccess " continuing"
+	echo ""
+}
+
+echoAlert () {
+	echo -e "\033[1;31m$1"
+	tput sgr0
+}
+
+echoSuccess () {
+	echo -e "\033[1;32m$1"
+	tput sgr0
+}
+
+echoWarning () {
+	echo -e "\033[1;33m$1"
+	tput sgr0
+}
+
+echoHighlight () {
+	echo -e "\033[1;36m$1"
+	tput sgr0
+}
+
+run
+
